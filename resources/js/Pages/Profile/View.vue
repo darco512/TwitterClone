f8b117e05c4e061ded36240f8f5591ba9f61880c
darco512 <darcohaha@gmail.com>
2024-02-08 18:50:25 +0200

    Implement uploading cover image on user

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -1,8 +1,48 @@
 <template>
     <AuthenticatedLayout>
-        <div class="w-[768px] mx-auto h-full overflow-auto">
-            <div class="relative  bg-white">
-                <img src="https://www.prodraw.net/fb_cover/images/fb_cover_65.jpg" class="w-full h-[200px] object-cover"/>
+        <div class="max-w-[768px] mx-auto h-full overflow-auto">
+            <div
+                v-show="showNotification && status === 'cover-image-updated'"
+                class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white"
+            >
+                Your cover image has been updated
+            </div>
+            <div
+                v-if="errors.cover"
+                class="my-2 py-2 px-3 font-medium text-sm bg-red-500 text-white"
+            >
+                {{ errors.cover }}
+            </div>
+            <div class="relative group bg-white">
+                <img :src="coverImageSrc || user.cover_url || '/image/default_cover.jpg'" class="w-full h-[200px] object-cover"/>
+                <div class="absolute top-2 right-2">
+                    <button v-if="!coverImageSrc" class="bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center opacity-0 group-hover:opacity-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+                        </svg>
+
+                        Update Cover Imange
+
+                        <input type="file" class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer z-2" @change="onCoverChange" />
+                    </button>
+                    <div v-else class="flex gap-2 opacity-0 group-hover:opacity-100">
+                        <button 
+                            class="bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center"
+                            @click="cancelCoverImage"
+                        >
+                            <XMarkIcon class="h-3 w-3 mr-2" />
+                            Cancel
+                        </button>
+                        <button 
+                            class="bg-gray-800 hover:bg-gray-900 text-gray-100 py-1 px-2 text-xs flex items-center"
+                            @click="submitCoverImage"    
+                        >
+                            <CheckCircleIcon class="h-3 w-3 mr-2" />
+                            Submit
+                        </button>
+                    </div>
+                </div>
                 <div class="flex">
                     <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" class="ml-[48px] w-[128px] h-[128px] -mt-[64px]" />
                     <div class="flex justify-between items-center flex-1 p-4">
@@ -61,15 +101,23 @@
     
   </template>
   
-  <script setup>
-  import { computed, ref } from 'vue'
-  import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-  import { usePage } from '@inertiajs/vue3';
+<script setup>
+import { computed, ref} from 'vue'
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import { useForm, usePage } from '@inertiajs/vue3';
 import AuthenticatedLayout from '../../Layouts/AuthenticatedLayout.vue';
 import TabItem from './Partials/TabItem.vue';
 import Edit from './Edit.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { XMarkIcon, CheckCircleIcon } from '@heroicons/vue/24/solid'
 
+const imagesForm = useForm({
+    avatar: null,
+    cover: null
+})
+
+const showNotification = ref(true)
+const coverImageSrc = ref('')
 const authUser = usePage().props.auth.user;
 
 const isMyProfile = computed(() => {
@@ -77,6 +125,7 @@ const isMyProfile = computed(() => {
 })
 
 const props = defineProps({
+    errors: Object,
     mustVerifyEmail: {
         type: Boolean,
     },
@@ -87,7 +136,35 @@ const props = defineProps({
         type: Object
     }
 });
-  </script>
+
+function onCoverChange(event) {
+    imagesForm.cover = event.target.files[0];
+    if(imagesForm.cover) {
+        const reader = new FileReader()
+        reader.onload = () => {
+            coverImageSrc.value = reader.result;
+        }
+        reader.readAsDataURL(imagesForm.cover)
+    }
+}
+
+function cancelCoverImage() {
+    imagesForm.cover=null;
+    coverImageSrc.value=null;
+}
+
+function submitCoverImage() {
+    imagesForm.post(route('profile.updateCover'), {
+        onSuccess: (user) => {
+
+            cancelCoverImage()
+            setTimeout(() =>{
+            showNotification.value = false;
+        }, 5000)
+        }
+    })
+}
+</script>
   
 <style lang="scss" scoped>
 
